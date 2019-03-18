4c438e63bcd30fba6cfe56dd0e68c41660734e1c
Virginia Lash <virginialash1@gmail.com>
2019-03-18 14:46:40 -0400

    form styling

--- breaddit-app/src/Components/NewPost.js
+++ breaddit-app/src/Components/NewPost.js
@@ -3,24 +3,14 @@ import React, { Component } from "react";
 class NewPost extends Component {
   render() {
     return (
-      <div>
         <form className="NewPost" onSubmit={this.props.fromForm}>
-          <input
-            type="text"
-            className="postTitle"
-            onChangeCapture={this.props.fromForm}
-          />
-          <textarea
-            cols="70"
-            rows="25"
-            placeholder="We're BREAD-y for your message"
-            onChangeCapture={this.props.fromFormCont}
-          />
+        <input class="form-control form-control-lg" type="text" className="postTitle" placeholder= "title" onChangeCapture={this.props.fromForm} />
+          <textarea class="form-control form-control-lg" cols="70" rows="25" placeholder="We're BREAD-y for your message" onChangeCapture={this.props.fromFormCont}/>
           <input type="submit" value="Submit" onClick={this.props.submission} />
         </form>
-      </div>
     );
   }
 }
+
 
 export default NewPost;
