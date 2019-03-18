869c1d8ef67a8b5b6c4869e90fd7baf3830b7f8d
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-03-18 10:47:38 -0400

    commit before live share

---
+++ breaddit-app/src/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Title goes here</h1>
+        <p>Post content goes here</p>
+      </div>
+    );
+  }
+}
+
+export default Posts;
