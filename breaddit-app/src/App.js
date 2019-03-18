11407ebfa33e645abf968c7d17936ba83381446d
Virginia Lash <virginialash1@gmail.com>
2019-03-18 11:02:00 -0400

    building out components adding them to app js page

--- breaddit-app/src/App.js
+++ breaddit-app/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from "react-router-dom"
 import './App.css';
 import Post from './Components/Post';
+import HomePage from './Components/Homepage';
 
 
 class App extends Component {
+  /*
+state and shit goes here
+  */
   render() {
     return (
       <div className="App">
-        <Post />
+        <header className="App-header">
+          <h1>Breaddit</h1>
+          <h2>WELCOME Bread LOVERS</h2>
+        </header>
+        <Route path="/" exact render={HomePage} />
+        <Route path="/bird/:name" render={Post}/>
+        <Route path="/create" render={Post}/>
       </div>
     );
   }
 }
-
 export default App;
