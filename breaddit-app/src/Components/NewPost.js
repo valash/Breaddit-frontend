0f281a12048b89e6c79fdd05eba3f6474b619e3e
Virginia Lash <virginialash1@gmail.com>
2019-03-19 14:59:03 -0400

    new post styling

--- breaddit-app/src/Components/NewPost.js
+++ breaddit-app/src/Components/NewPost.js
@@ -4,22 +4,26 @@ import "./NewPost.css";
 class NewPost extends Component {
   render() {
     return (
-      <div>
-        <form className="NewPost" onSubmit={this.props.submission} style={{marginLeft:"50%", marginRight:"50%"}}>
-          <input
-            type="text"
-            placeholder="title"
-            className="postTitle"
-            onChange={this.props.titleSet}
-          />
+      <div className="card">
+        <div className="card-body">
+        <form style={{height:"0"}}>
+        <label for="NewPost" onSubmit={this.props.submission}className="grey-text font-weight-light" style={{display:"block"}}> Title</label>
+          <input type="text" placeholder="title"  onChange={this.props.titleSet} style={{margin:"auto", width:"500px"}}/>
+              <label for="postBody" className="grey-text font-weight-light" onSubmit={this.props.submission}style={{display:"block"}}>Body:</label>
           <textarea
-            rows="14"
+            rows="30"
             placeholder="We're BREAD-y for your message"
             onChange={this.props.contentSet}
-          />
-          <input type="submit" value="Submit" />
-        </form>
+              style={{ margin:"80px 541.441px 0px 566.128px;", display:"block;"
+               }}
+          />       
+          <form>
+            <input type="submit" value="Submit" />
+          </form>
+            </form>
+          
         </div>
+      </div>
     );
   }
 }
