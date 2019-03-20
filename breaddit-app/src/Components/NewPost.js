a2fcb7ab1f59b4c0653b693c719f8de3b3d2d6f1
Virginia Lash <virginialash1@gmail.com>
2019-03-20 10:52:19 -0400

    new toast styling

--- breaddit-app/src/Components/NewPost.js
+++ breaddit-app/src/Components/NewPost.js
@@ -6,9 +6,9 @@ class NewPost extends Component {
     return (
       <div className="card">
         <div className="card-body">
-        <form style={{height:"0"}}>
+        <form style={{height:"0", fontSize:"40px"}}>
         <label for="NewPost" onSubmit={this.props.submission}className="grey-text font-weight-light" style={{display:"block"}}> Title</label>
-          <input type="text" placeholder="title"  onChange={this.props.titleSet} style={{margin:"auto", width:"500px"}}/>
+            <input type="text" placeholder="title" onChange={this.props.titleSet} style={{ margin: "auto", width: "500px", borderRadius: "9px",}}/>
               <label for="postBody" className="grey-text font-weight-light" onSubmit={this.props.submission}style={{display:"block"}}>Body:</label>
           <textarea
             placeholder="We're BREAD-y for your message"
@@ -17,7 +17,7 @@ class NewPost extends Component {
                }}
           />       
           <form>
-              <input type="submit" style={{ backgroundColor: 'grey', border:"0px", color:"white" }} value="Submit" />
+              <input type="submit" style={{ backgroundColor: 'White', color: "Black", height:"34px", width:"76px", borderRadius:"9px", fontSize:"15px" }} value="Submit" />
           </form>
             </form>
           
