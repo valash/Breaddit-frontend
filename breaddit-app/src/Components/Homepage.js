cab508931e5509bdc3da556421e7d312a3f4e24b
Virginia Lash <virginialash1@gmail.com>
2019-03-20 15:30:27 -0400

    homepage styling

--- breaddit-app/src/Components/Homepage.js
+++ breaddit-app/src/Components/Homepage.js
@@ -7,10 +7,11 @@ class Homepage extends Component {
     const { posts } = this.props;
     return (
       <div className="card-body" style={{textAlign:"center"}} > 
+      <h1 style={{marginTop:"30p"}}>BAKED BREAD</h1>
         <h3>
           {posts.map(post => (
             <Link to={"/post/" + post.title} >
-              <div className="card-body" style={{ border: "1px solid black", borderRadius: "30px", boxShadow:"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19", height:"150px", textAlign:"center", width:"600px", margin:"auto", backgroundColor:"yellow"}} key={post.id}><div className="card-body">{post.title}</div>
+              <div className="card-body" style={{ border: "1px solid black", borderRadius: "30px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19", height: "150px", textAlign: "center", width: "60%", marginTop: "40px", marginLeft: "20%", backgroundColor:" rgba(0, 0, 0, 0.3)", paddingTop:"50px", fontSize:'70px'}} key={post.id}><div className="card-body">{post.title}</div>
               </div>
             </Link>
            
