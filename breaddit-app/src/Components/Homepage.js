9fdb9fa98cc33b5ca01ed2f266781ac974758f24
Virginia Lash <virginialash1@gmail.com>
2019-03-20 15:23:01 -0400

    YES MADE THE WHITE STUFF GO AWAY

--- breaddit-app/src/Components/Homepage.js
+++ breaddit-app/src/Components/Homepage.js
@@ -10,8 +10,10 @@ class Homepage extends Component {
         <h3>
           {posts.map(post => (
             <Link to={"/post/" + post.title} >
-              <div style={{ border: "5px solid black", margin:"20%", borderRadius:"5px" }} key={post.id}>{post.title}</div>
+              <div className="card-body" style={{ border: "1px solid black", borderRadius: "30px", boxShadow:"0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19", height:"150px", textAlign:"center", width:"600px", margin:"auto", backgroundColor:"yellow"}} key={post.id}><div className="card-body">{post.title}</div>
+              </div>
             </Link>
+           
           ))}
         </h3>
         {/* look up how to put post previews */}
