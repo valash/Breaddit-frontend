49724e16da88551f3d09207eb5b69ba41b55fcfa
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-03-20 17:54:20 -0400

    single comment renders on page

--- breaddit-app/src/Components/Comment.js
+++ breaddit-app/src/Components/Comment.js
@@ -7,21 +7,27 @@ class Comment extends Component {
       comments: []
     };
     this.commentCreate = this.commentCreate.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   commentCreate(event) {
     this.setState({ comments: event.target.value });
+    event.preventDefault();
+  }
+  handleSubmit(event) {
+    console.log(event.target.value);
+    event.preventDefault();
   }
   render() {
     return (
       <div>
         <h4>I am from the Comment Component!</h4>
-        <form className="makeComment" onSubmit={this.state.submission}>
+        <form className="makeComment" onSubmit={this.handleSubmit}>
           <textarea
             rows="3"
             cols="50"
             placeholder="comment"
             maxLength="125"
-            onChange={this.state.commentCreate}
+            onChange={this.commentCreate}
             name={this.state.comments}
           />
           <input type="submit" value="Comment!" />
