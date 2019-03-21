854f3f212c2138ed21c3a08397987e06816a9502
Virginia Lash <virginialash1@gmail.com>
2019-03-21 14:07:49 -0400

    fixed post styling

--- breaddit-app/src/Components/Post.js
+++ breaddit-app/src/Components/Post.js
@@ -1,51 +1,63 @@
-import React, { Component } from "react";
-import Comment from "./Comment";
+import React, {Component} from 'react';
+import Comment from './Comment';
 
 class Post extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
-  render() {
-    return (
-      <div className="card text-center" style={{width:"50%", margin:"auto", marginTop:"10%"}}>
-        {/* <div class="card-header">{this.props.title}</div> */}
-        <div className="card-body">
-        <h5 className="card-title">{this.props.title}</h5>
-        <p className="card-text">{this.props.content}</p>
-          <Comment
-            comments={this.props.comments}
-            commentCreate={this.props.commentCreate}
-          />
-          <div className="card-footer text-muted" style={{ textAlign: "center" }}><h4>Burn it Butter it</h4></div>
-          </div>
+	constructor(props) {
+		super(props);
+		this.state = {
+			posts: []
+		};
+	}
+	render() {
+		return (
+			<div
+				className='card text-center'
+				style={{
+					width: '50%',
+					margin: 'auto',
+					marginTop: '10%'
+				}}>
+				<div className='card-body'>
+					{this.props.title}
+					{this.props.content}
 
-          
-</div>
-    );
-  }
+					<Comment
+						comments={this.props.comments}
+						commentCreate={
+							this.props.commentCreate
+						}
+					/>
+					<div
+						className='card-footer text-muted'
+						style={{
+							textAlign: 'center',
+							background: 'none',
+							borderRadius: 'none',
+							height: '55px'
+						}}>
+						<p className='pull-left'>
+							<span
+								className='glyphicon glyphicon-thumbs-down'
+								style={{
+									margin: '9px'
+								}}
+							/>
+							Burn It
+						</p>
+						<p className='pull-right'>
+							<span
+								className='glyphicon glyphicon-thumbs-up'
+								style={{
+									margin: '9px'
+								}}
+							/>
+							Butter It
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Post;
-
-
-
-
-
-//  <img src={require('../images/logo.png')} alt="reddit bread" style={{ height: "40px" }} ></img> */}
-
-// <div class="card bg-light mb-3" style="max-width: 18rem;">
-//   <div class="card-header">Header</div>
-//   <div class="card-body">
-//     <h5 class="card-title">Light card title</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//   </div>
-// </div>
-//  */
-
-// style = {{ marginLeft: "30%", marginTop: "15%" }}>
-
-//  <img className="card-top" src={require('../images/bread.jpg')} alt="Card cap" style={{height:"301px"}}/>  
-
--- breaddit-app/src/Components/SignUp.js
+++ breaddit-app/src/Components/SignUp.js
@@ -1,36 +1,66 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import './SignUp.css';
 
 class SignUp extends Component {
-  render() {
-    return (
-      <div className="card">
-      <div className="card-body">
-        <form style={{height:"0", fontSize:"40px",display:"grid"}}>
-       
-          <p className="h4 text-center py-4" style={{fontSize:"-webkit-xx-large",marginTop:"10%"}}>SIGN UP</p>
-            {/* <div className="whole-signup" style={{ fontSize: "-webkit-xxx-large"}}> */}
-            <div className="email" style={{margin:"auto"}}>
-          <label for="defaultFormCardEmailEx" className="grey-text font-weight-light">
-            <input type="text" placeholder="email" className="form-control"/>
-          </label>
-          </div>
-            <div className="password" style={{ margin: "auto" }}>
-            <label for="defaultFormCardPasswordEx" className="grey-text font-weight-heavy">
-                <input type="text" placeholder="password" className="form-control"/>
-          </label>
-            </div>
-            <div class="text-center py-4 mt-3">
-              <button className="btn btn-outline-purple" type="submit">Submit</button>
-              </div>
-            {/* </div> */}
-        </form>
-      </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='card'>
+				<div className='card-body'>
+					<form
+						style={{
+							height: '0',
+							fontSize: '40px',
+							display: 'grid'
+						}}>
+						<p
+							className='h4 text-center py-4'
+							style={{
+								fontSize:
+									'-webkit-xx-large',
+								marginTop: '10%'
+							}}>
+							SIGN UP
+						</p>
+						{/* <div className="whole-signup" style={{ fontSize: "-webkit-xxx-large"}}> */}
+						<div
+							className='email'
+							style={{margin: 'auto'}}>
+							<label
+								for='defaultFormCardEmailEx'
+								className='grey-text font-weight-light'>
+								<input
+									type='text'
+									placeholder='email'
+									className='form-control'
+								/>
+							</label>
+						</div>
+						<div
+							className='password'
+							style={{margin: 'auto'}}>
+							<label
+								for='defaultFormCardPasswordEx'
+								className='grey-text font-weight-heavy'>
+								<input
+									type='text'
+									placeholder='password'
+									className='form-control'
+								/>
+							</label>
+						</div>
+						<div class='text-center py-4 mt-3'>
+							<button
+								className='btn btn-outline-purple'
+								type='submit'>
+								Submit
+							</button>
+						</div>
+						{/* </div> */}
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
 
-
 export default SignUp;
-
