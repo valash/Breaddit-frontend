a201aabe74c3ffbda35583272b9e39854fdcd2c1
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-03-21 09:41:44 -0400

    added concat to form

--- breaddit-app/src/Components/Comment.js
+++ breaddit-app/src/Components/Comment.js
@@ -3,23 +3,29 @@ import React, { Component } from "react";
 class Comment extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      comments: []
-    };
+    this.state = { comments: [] };
     this.commentCreate = this.commentCreate.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.addComment = this.addComment.bind(this);
   }
   commentCreate(event) {
     event.preventDefault();
-    this.setState({ comments: event.target.value });
+    this.setState({ comments: [event.target.value] });
   }
   handleSubmit(event) {
     event.preventDefault();
     console.log(this.state.comments);
   }
+  addComment = () => {
+    this.setState(state => {
+      const comments = state.comments.concat(state.value);
+      return comments;
+    });
+  };
   render() {
     return (
       <div>
+        {/* Need to find a way to push input into this array i just created so that multiple comments can appear on the page */}
         <h4>I am from the Comment Component!</h4>
         <form className="makeComment" onSubmit={this.handleSubmit}>
           <textarea
@@ -28,12 +34,14 @@ class Comment extends Component {
             placeholder="comment"
             maxLength="125"
             onChange={this.commentCreate}
-            name={this.state.comments}
+            value={this.state.comments}
           />
-          <input type="submit" value="Comment!" />
+          <input type="submit" value="Comment!" onClick={this.addComment} />
         </form>
         <p>New comments should appear here:</p>
-        {this.state.comments}
+        {this.state.comments.map(comment => (
+          <p>{comment}</p>
+        ))}
       </div>
     );
   }
