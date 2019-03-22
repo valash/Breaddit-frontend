609ec223678e3a1441b61fe4c600f4d382663f20
Virginia Lash <virginialash1@gmail.com>
2019-03-22 10:47:39 -0400

    styling on post and home page and new toast

--- breaddit-app/src/Components/Post.js
+++ breaddit-app/src/Components/Post.js
@@ -1,57 +1,54 @@
-import React, { Component } from "react";
-import Comment from "./Comment";
+import React, {Component} from 'react';
+import Comment from './Comment';
 
 class Post extends Component {
-  render() {
-    return (
-      <div
-        className="card text-center"
-        style={{
-          width: "50%",
-          margin: "auto",
-          marginTop: "10%"
-        }}
-      >
-        <div className="card-body">
-          {this.props.title}
-          {this.props.content}
-
-          <Comment
-            comments={this.props.comments}
-            commentCreate={this.props.commentCreate}
-          />
-          <div
-            className="card-footer text-muted"
-            style={{
-              textAlign: "center",
-              background: "none",
-              borderRadius: "none",
-              height: "55px"
-            }}
-          >
-            <p className="pull-left">
-              <span
-                className="glyphicon glyphicon-thumbs-down"
-                style={{
-                  margin: "9px"
-                }}
-              />
-              Burn It
-            </p>
-            <p className="pull-right">
-              <span
-                className="glyphicon glyphicon-thumbs-up"
-                style={{
-                  margin: "9px"
-                }}
-              />
-              Butter It
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div
+				className='card text-center'
+				style={{
+					width: '50%',
+					margin: 'auto',
+					marginTop: '10%'
+				}}>
+				<div className='card-header'>
+					{this.props.title}Title
+					<div className='card-body'>
+						Content
+						<Comment />
+						{this.props.content}
+						<div
+							className='card-footer text-muted'
+							style={{
+								textAlign: 'center',
+								background: 'none',
+								borderRadius: 'none',
+								height: '55px'
+							}}>
+							<p className='pull-left'>
+								<span
+									className='glyphicon glyphicon-thumbs-down'
+									style={{
+										margin: '9px'
+									}}
+								/>
+								Burn It
+							</p>
+							<p className='pull-right'>
+								<span
+									className='glyphicon glyphicon-thumbs-up'
+									style={{
+										margin: '9px'
+									}}
+								/>
+								Butter It
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Post;
